212e685acad8a9ca252ae64e27022f657b9e6f3d
suisho <suisho000000@gmail.com>
2013-09-06 00:20:18 +0900

    複数キー対応したい

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var EventEmitter = require('events').EventEmitter;
+var _ = require('underscore')
 
 var keypress = require("keypress")
 
 var pressed = false
+var pressedKeys = {}
+var pressedKeysStack = []
 var lastPressed = 0
 var keypressCount = 0
 var immediateRpeat = 0 // for debug
@@ -16,12 +19,19 @@ function keydownup(stream){
   // keypress handling
   kp = keypress(stream)
   stream.on('keypress', function(ch, key){
-    if(pressed == false){
+    if(!pressedKeys[ch]){
       stream.emit('keydown', ch, key)
     }
     var d = new Date()
     pressed = true
-    var beforeLastPressed = lastPressed
+    // push key data
+    pressedKeys[ch] = key
+    pressedKeys[ch].lastPressed =lastPressed
+    // stacking
+    pressedKeysStack.unshift(key)
+    pressedKeysStack = _.uniq(pressedKeysStack)
+    log.debug(pressedKeysStack)
+    // last time
     lastPressed = d.getTime()
     keypressCount++
   })
@@ -41,10 +51,16 @@ function keydownup(stream){
     if(keypressCount == 1){
       threshold = threshold * 2
     }
+    // 複数キー対応するにはこれ個別でやらんといかんなあ。
     if(interval > threshold){
       log.debug(interval)
-      pressed = false
-      stream.emit('keyup')
+      if(pressedKeysStack.length == 0){
+        pressed = false
+      }
+      var releasedKey = pressedKeysStack.pop()
+      var key = pressedKeys[releasedKey]
+      delete pressedKeys[releasedKey]
+      stream.emit('keyup', releasedKey, key)
       keypressCount = 0
 
     }
--- tester.js
+++ tester.js
@@ -1,14 +1,15 @@
-var keydownup = require("./keyemulator")
+var keydownup = require("./index")
 keydownup(process.stdin)
 
-process.stdin.on('keydown', function(){
-  console.log("keydown")
+process.stdin.on('keydown', function(ch, key){
+  console.log("____keydown", ch, key)
+
 })
-process.stdin.on('keyup', function(){
-  console.log("keyup")
+process.stdin.on('keyup', function(ch, key){
+  console.log("^^^keyup", ch, key)
 })
 process.stdin.on('keypress', function(ch, key){
-  console.log("keypress")
+  //console.log("keypress")
   if(key && key.name == "c" && key.ctrl){
     console.log("exit")
     process.exit()
